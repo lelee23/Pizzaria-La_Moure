59e8072627a0079643a1eaedc285f6a6de6aa2f9
lelee23 <113265900+lelee23@users.noreply.github.com>
2022-11-28 20:49:52 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
-const burguer = document.querySelector('.burguer');
-const menu = document.querySelector('.menu')
-const linha1= document.querySelector('.linha1');
-const linha2= document.querySelector('.linha2');
-const linha3= document.querySelector('.linha3');
+// const burguer = document.querySelector('.burguer');
+// const menu = document.querySelector('.menu')
+// const linha1= document.querySelector('.linha1');
+// const linha2= document.querySelector('.linha2');
+// const linha3= document.querySelector('.linha3');
 
-burguer.addEventListener('click', function(){
-    menu.classList.toggle('show');
-    linha1.classList.toggle('linha1');
-    linha2.classList.toggle('linha2');
-    linha3.classList.toggle('linha3');
+// burguer.addEventListener('click', function(){
+//     menu.classList.toggle('show-menu');
+//     linha1.classList.toggle('linha1');
+//     linha2.classList.toggle('linha2');
+//     linha3.classList.toggle('linha3');
 
-})
+// })
+
+document.querySelector(".burger").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
